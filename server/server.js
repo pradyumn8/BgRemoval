7661542c695e0d2d0d04c42d262922c1c0e6d04f
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-11-09 15:13:31 +0530

    ignore

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-import 'dotenv/config';
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './configs/mongodb.js'
+import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
+
+
 
 // App Config
 const PORT = process.env.PORT || 4000
 const app = express()
 await connectDB()
 
-// Intialize Middlewares
+// Initalize Middlewares
 app.use(express.json())
 app.use(cors())
 
 // API routes
-app.get('/',(req,res)=>res.send("API Working"))
+app.get('/',(req,res)=>res.send('API Working'))
+app.use('/api/user',userRouter)
 
-app.listen(PORT,()=>console.log("Server Running on port "+PORT))
+app.listen(PORT, ()=> console.log("Server Running on port "+PORT))
